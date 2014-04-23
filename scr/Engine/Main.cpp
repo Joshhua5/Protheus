b142738ae4a31beec0b106b3fcb3c4f66f6953ad
Joshua Waring <Joshhua123@gmail.com>
2014-04-23 21:34:45 +0800

    Added Embedded Lua

---
+++ scr/Engine/Main.cpp
@@ -0,0 +1,39 @@
+#include "Main.h"
+ 
+
+int main(int argc, char* args[])
+{
+	Pro::Core* core = new Pro::Core();
+
+	SDL_Rect dim;
+	dim.x = 200;
+	dim.y = 200;
+	dim.w = 800;
+	dim.h = 600;
+	core->setWindowDimensions(dim);
+	core->setWindowTitle("Core");
+	core->start();
+	 
+	if (core->loadSpriteSheet(".\\textures\\SpriteSheet.PNG", "textures\\SpriteSheet.txt") == false)
+		std::cout << "Error Loading Sprite Sheet";
+	if (core->loadSpriteAnimations(".\\textures\\animations.txt") == false)
+		std::cout << "Error Loading Sprite Animations";
+	if (core->loadScene(".\\levels\\demoLevel.txt", ".\\levels\\demoLevelData.txt") == false)
+		std::cout << "Error Loading Scene";
+
+	// Load Camera 
+	Pro::Camera camera;
+	camera.setDimensions(0, 0, 18, 18);
+	core->addCamera(camera, "camera1");
+	core->setActiveCamera("camera1");
+	 
+	 
+	while (!SDL_QuitRequested())
+	{
+		core->run(); 
+	}
+	core->cleanup();
+	delete core;
+	return 0; 
+}
+
