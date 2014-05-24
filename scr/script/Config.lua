7528639472ca33ae1842af94b037de56958e0c8f
JoshuaWaring <Joshhua123@gmail.com>
2014-05-24 12:44:32 +0800

    Added instructions on how to use Git Removed the duplicated game files
    Signed-off-by: JoshuaWaring <Joshhua123@gmail.com>

--- scr/script/Config.lua
+++
@@ -1,8 +0,0 @@
-
-resource_path = "script\\Resources.lua"
-main_path = "script\\Main.lua"
-screen_width = 800
-screen_height = 600
-screen_position_x = 100
-screen_position_y = 100
-title = "Protheus" 
--- scr/script/Resources.lua
+++
@@ -1,4 +0,0 @@
-
-load_spritesheet(".\\textures\\SpriteSheet.PNG", ".\\textures\\SpriteSheet.txt")
-load_spriteanimations(".\\textures\\animations.txt")
-load_scene(".\\levels\\demoLevel.txt", ".\\levels\\demoLevelData.txt")
