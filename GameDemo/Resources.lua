f8e22f330c5c2cefd4b33c8a0ab59a91bfa1790c
Joshua Waring <Joshhua123@gmail.com>
2014-07-18 13:29:05 +0800

    Cleaned up project repo - Deleted File Packer - Deleted Game File

--- GameDemo/Resources.lua
+++
@@ -1,5 +0,0 @@
-
-
-load_spritesheet(".\\textures\\SpriteSheet.PNG", ".\\textures\\SpriteSheet.txt")
-load_spriteanimations(".\\textures\\animations.txt")
-scene_create():loadScene(".\\levels\\demoLevel.txt", ".\\levels\\demoLevelData.txt")
